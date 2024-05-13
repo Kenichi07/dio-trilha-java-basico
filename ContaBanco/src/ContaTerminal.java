afc0ed83a5aec75eb2a7908e0770581d221563d1
Vinicius Huang <viniciushuangbr@gmail.com>
2024-05-13 17:24:52 -0300

    segundo commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor, Digite seu primeiro nome: ");
+        String nome = scanner.nextLine();
+
+        // System.out.println("Por favor, Digite o número da sua agência: ");
+        // String agencia = scanner.nextLine();
+
+        // System.out.println("Por favor, Digite o seu saldo: ");
+        // double saldo = scanner.nextDouble();
+
+        // System.out.println("Por favor, Digite o número da sua conta: ");
+        // int numConta = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Olá " + nome + "");
     }
 }
