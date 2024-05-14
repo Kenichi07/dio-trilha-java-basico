a69b57949aafabb7bbc83865a85b2382dd96194e
Vinicius Huang <viniciushuangbr@gmail.com>
2024-05-13 23:53:41 -0300

    terceiro commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,19 +3,21 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Por favor, Digite seu primeiro nome: ");
+        System.out.println("Por favor, Digite seu nome: ");
         String nome = scanner.nextLine();
 
-        // System.out.println("Por favor, Digite o número da sua agência: ");
-        // String agencia = scanner.nextLine();
+        System.out.println("Por favor, Digite o número da sua agência: ");
+        String agencia = scanner.nextLine();
 
-        // System.out.println("Por favor, Digite o seu saldo: ");
-        // double saldo = scanner.nextDouble();
+        System.out.println("Por favor, Digite o seu saldo: ");
+        double saldo = scanner.nextDouble();
 
-        // System.out.println("Por favor, Digite o número da sua conta: ");
-        // int numConta = scanner.nextInt();
+        System.out.println("Por favor, Digite o número da sua conta: ");
+        int numConta = scanner.nextInt();
+        
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
         scanner.close();
-
-        System.out.println("Olá " + nome + "");
     }
 }
