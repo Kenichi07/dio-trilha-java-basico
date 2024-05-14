d3500c794331188b8a719e3ded29fb27db7c02fa
Vinicius Huang <viniciushuangbr@gmail.com>
2024-05-14 09:33:15 -0300

    adicionando outras formas de receber os dados

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,4 +1,6 @@
+import java.io.Console;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
@@ -25,5 +27,29 @@ public class ContaTerminal {
         //     + ", conta ".concat(Integer.toString(numConta)) + " e seu saldo ".concat(Double.toString(saldo)) + " já está disponível para saque.");
 
         scanner.close();
+
+        // Console console = System.console();
+        // System.out.println("Por favor, Digite seu nome: ");
+        // String nome = console.readLine();
+
+        // System.out.println("Por favor, Digite o número da sua agência: ");
+        // String agencia = console.readLine();
+        // System.out.println("Por favor, Digite o seu saldo: ");
+        // String saldo = console.readLine();
+
+        // System.out.println("Por favor, Digite o número da sua conta: ");
+        // String numConta = console.readLine();
+
+        // System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+        //      + agencia + ", conta " + numConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        // String nome = JOptionPane.showInputDialog("Por favor, Digite seu nome: ");
+        // String agencia = JOptionPane.showInputDialog("Por favor, Digite o número da sua agência: ");
+        // String saldo = JOptionPane.showInputDialog("Por favor, Digite o seu saldo: ");
+        // String numConta = JOptionPane.showInputDialog("Por favor, Digite o número da sua conta: ");
+
+        // JOptionPane.showMessageDialog(null, "Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+        //    + agencia + ", conta " + numConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
