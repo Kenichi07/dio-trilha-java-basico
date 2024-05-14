da8eba36241d80da80fe2344f2f9ed630c55d354
Vinicius Huang <viniciushuangbr@gmail.com>
2024-05-14 00:18:59 -0300

    quarto commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
@@ -14,9 +15,14 @@ public class ContaTerminal {
 
         System.out.println("Por favor, Digite o número da sua conta: ");
         int numConta = scanner.nextInt();
-        
+
+        // USANDO SÓ O SINAL + PARA CONCATENAÇÃO
         System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
                 + agencia + ", conta " + numConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        // USANDO O MÉTODO CONCAT
+        // System.out.println("Olá ".concat(nome) + ", obrigado por criar uma conta em nosso banco, sua agência é ".concat(agencia) 
+        //     + ", conta ".concat(Integer.toString(numConta)) + " e seu saldo ".concat(Double.toString(saldo)) + " já está disponível para saque.");
 
         scanner.close();
     }
